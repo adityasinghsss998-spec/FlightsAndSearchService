70fb8a1def84b6a145190d8d2537c3e6660a697e
aditya <adityasingh.sss998@gmail.com>
2026-01-17 12:31:53 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express=require("express");
+const bodyParser= require("body-parser")
 const {PORT}= require('./config/serverconfig')
 const setUpAndStartServer = async()=>{
     //create the express object
     const app=express();
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended : true}));
+    
     app.listen(3000,()=>{
       console.log("server started at port",PORT);
       
